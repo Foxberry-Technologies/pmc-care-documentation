e87101187130a37705767ca5f6e16280eb77c702
Shanu Jha <shanu.j@foxberry.in>
2023-08-11 18:01:22 +0530

    Update url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/logo.jpg',
 
   // Set the production url of your site here
-  url: 'https://foxberry-technologies.github.io/pmc-care-documentation/',
+  url: 'https://foxberry-technologies.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/pmc-care-documentation',
